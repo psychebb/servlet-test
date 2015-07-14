c29d8c5620a921d44b3204b094e44852be8d7c82
Rui <phoenix.bian@gmail.com>
2015-07-14 22:43:54 +0800

    render a jsp page when doGet

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,9 +10,9 @@ public class HelloWorld extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws IOException {
-        response.getOutputStream().write("helloworld".getBytes());
-
+                         HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("method", "get");
+        request.getRequestDispatcher("/index.jsp").forward(request,response);
     }
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: rbian
-  Date: 7/14/15
-  Time: 10:08 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title></title>
+    <title>Servlet Learning</title>
   </head>
   <body>
-
+  This is a ${method} page
   </body>
 </html>
